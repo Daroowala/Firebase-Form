c8e98ce7118e0db279d31361bb152c6235c4e2cb
my name <blueshaperd3@gmail.com>
2024-09-10 03:08:37 +0500

    initalize and authantication

--- app.js
+++ app.js
@@ -2,11 +2,8 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+ 
   const firebaseConfig = {
     apiKey: "AIzaSyB6uesyXFbo8rDFKcbJj6O3loYSQY8LFow",
     authDomain: "fir-form-5e9ba.firebaseapp.com",
@@ -20,3 +17,7 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  console.log("app=>",app);
+
+  const auth = getAuth(app);
+  console.log("auth=>",auth);
